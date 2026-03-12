4546c169a944da4a5b691e85038030cfea4ed371
chj Win7Evernote VM <chjfth@gmail.com>
2026-03-12 09:54:12 +0800

    CHHI updates

--- mswin/LMAccess.itc.h
+++ mswin/LMAccess.itc.h
@@ -1,11 +1,64 @@
-#pragma once
+#ifndef __CHHI__LMAccess_h_20260312_
+#define __CHHI__LMAccess_h_20260312_
 
-#include "../itc/InterpretConst.h"
+#include <InterpretConst.h>
 
 namespace itc {
 
-
-extern CInterpretConst ACCESS_xxx;
+extern const CInterpretConst ACCESS_xxx;
 
 
 } // namespace itc
+
+
+/*
+////////////////////////////////////////////////////////////////////////////
+ ___                 _                           _        _   _             
+|_ _|_ __ ___  _ __ | | ___ _ __ ___   ___ _ __ | |_ __ _| |_(_) ___  _ __  
+ | || '_ ` _ \| '_ \| |/ _ \ '_ ` _ \ / _ \ '_ \| __/ _` | __| |/ _ \| '_ \ 
+ | || | | | | | |_) | |  __/ | | | | |  __/ | | | || (_| | |_| | (_) | | | |
+|___|_| |_| |_| .__/|_|\___|_| |_| |_|\___|_| |_|\__\__,_|\__|_|\___/|_| |_|
+              |_|                                                           
+////////////////////////////////////////////////////////////////////////////
+*/
+// ++++++++++++++++++ Implementation Below ++++++++++++++++++
+//
+#if defined(LMAccess_itc_IMPL) || (defined CHHI_ALL_IMPL && !defined CHHI_ALL_IMPL_HIDE_LMAccess_itc) // [IMPL] begin
+// [IMPL] //
+// [IMPL] //
+// Include system/OS headers here
+//#include <stdio.h>
+// [IMPL] //
+// [IMPL] //
+namespace itc {
+
+// More ctor reference: My evclip 20250411.v1
+
+
+const Enum2Val_st e2v_ACCESS_xxx[] =
+{
+	ITC_NAMEPAIR(ACCESS_READ),   // 0x01
+	ITC_NAMEPAIR(ACCESS_WRITE),  // 0x02
+	ITC_NAMEPAIR(ACCESS_CREATE), // 0x04
+	ITC_NAMEPAIR(ACCESS_EXEC),   // 0x08
+	ITC_NAMEPAIR(ACCESS_DELETE), // 0x10
+	ITC_NAMEPAIR(ACCESS_ATRIB),  // 0x20
+	ITC_NAMEPAIR(ACCESS_PERM),   // 0x40
+};
+const SingleBit2Val_st b2v_ACCESS_GROUP[] =
+{
+	ITC_NAMEPAIR(ACCESS_GROUP)
+};
+const CInterpretConst ACCESS_xxx(ITCF_HEX1B, e2v_ACCESS_xxx, ARRAYSIZE(e2v_ACCESS_xxx),
+	b2v_ACCESS_GROUP, ARRAYSIZE(b2v_ACCESS_GROUP),
+	nullptr, 0);
+
+
+
+} // namespace itc
+// [IMPL] //
+// [IMPL] //
+#endif // [IMPL] end
+
+
+#endif // CHHI__LMAccess_h_20260312_
--- mswin/NTSecAPI.itc.h
+++ mswin/NTSecAPI.itc.h
@@ -1,12 +1,77 @@
-#pragma once
+#ifndef __CHHI__NTSecAPI_h_20260312_
+#define __CHHI__NTSecAPI_h_20260312_
 
-#include "../itc/InterpretConst.h"
+#include <InterpretConst.h>
 
 namespace itc {
 
+extern const CInterpretConst itc_SECURITY_LOGON_TYPE;
 
-extern CInterpretConst itc_SECURITY_LOGON_TYPE;
-
-extern CInterpretConst LOGON_xxx_UserFlags;
+extern const CInterpretConst LOGON_xxx_UserFlags;
 
 } // namespace itc
+
+
+/*
+////////////////////////////////////////////////////////////////////////////
+ ___                 _                           _        _   _             
+|_ _|_ __ ___  _ __ | | ___ _ __ ___   ___ _ __ | |_ __ _| |_(_) ___  _ __  
+ | || '_ ` _ \| '_ \| |/ _ \ '_ ` _ \ / _ \ '_ \| __/ _` | __| |/ _ \| '_ \ 
+ | || | | | | | |_) | |  __/ | | | | |  __/ | | | || (_| | |_| | (_) | | | |
+|___|_| |_| |_| .__/|_|\___|_| |_| |_|\___|_| |_|\__\__,_|\__|_|\___/|_| |_|
+              |_|                                                           
+////////////////////////////////////////////////////////////////////////////
+*/
+// ++++++++++++++++++ Implementation Below ++++++++++++++++++
+//
+#if defined(NTSecAPI_itc_IMPL) || (defined CHHI_ALL_IMPL && !defined CHHI_ALL_IMPL_HIDE_NTSecAPI_itc) // [IMPL] begin
+// [IMPL] //
+// [IMPL] //
+// Include system/OS headers here
+#include <stdio.h>
+// [IMPL] //
+// [IMPL] //
+namespace itc {
+
+// More ctor reference: My evclip 20250411.v1
+
+const Enum2Val_st _e2v_SECURITY_LOGON_TYPE[] =
+{
+    ITC_NAMEPAIR(UndefinedLogonType),// =0 This is used to specify an undefied logon type
+    ITC_NAMEPAIR(Interactive),       // =2 Interactively logged on (locally or remotely)
+    ITC_NAMEPAIR(Network),              // Accessing system via network
+    ITC_NAMEPAIR(Batch),                // Started via a batch queue
+    ITC_NAMEPAIR(Service),              // Service started by service controller
+    ITC_NAMEPAIR(Proxy),                // Proxy logon
+    ITC_NAMEPAIR(Unlock),               // Unlock workstation
+    ITC_NAMEPAIR(NetworkCleartext),     // Network logon with cleartext credentials
+    ITC_NAMEPAIR(NewCredentials),       // Clone caller, new default credentials
+    
+    //The types below only exist in Windows XP and greater
+    ITC_NAMEPAIR(RemoteInteractive),  // Remote, yet interactive. Terminal server
+    ITC_NAMEPAIR(CachedInteractive),  // Try cached credentials without hitting the net.
+    
+    // The types below only exist in Windows Server 2003 and greater
+    ITC_NAMEPAIR(CachedRemoteInteractive), // Same as RemoteInteractive, this is used internally for auditing purpose
+    ITC_NAMEPAIR(CachedUnlock),        // Cached Unlock workstation
+};
+const CInterpretConst itc_SECURITY_LOGON_TYPE(_e2v_SECURITY_LOGON_TYPE, ITCF_SINT);
+
+
+const SingleBit2Val_st _b2v_LogonUserFlags[] =
+{
+	ITC_NAMEPAIR(LOGON_OPTIMIZED),
+	ITC_NAMEPAIR(LOGON_WINLOGON),    
+	ITC_NAMEPAIR(LOGON_PKINIT),      
+	ITC_NAMEPAIR(LOGON_NO_OPTIMIZED),
+};
+const CInterpretConst LOGON_xxx_UserFlags(_b2v_LogonUserFlags, ITCF_HEX2B);
+
+
+} // namespace itc
+// [IMPL] //
+// [IMPL] //
+#endif // [IMPL] end
+
+
+#endif // CHHI__NTSecAPI_h_20260312_
